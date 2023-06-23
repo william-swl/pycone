eaf6b999b42c7e09d2746a5e1b7aa042b7b47d99
william <william_swl@163.com>
2023-06-23 17:00:46 +0800

    feat: add tox

---
+++ pycone/__init__.py
@@ -0,0 +1,24 @@
+"""
+The pycone package provides useful functions
+"""
+
+
+def flatten(lst):
+    """
+    flatten a list
+    """
+    result = []
+    for item in lst:
+        if isinstance(item, list):
+            result.extend(flatten(item))
+        else:
+            result.append(item)
+    return result
+
+
+def list_join(*args, sep=""):
+    """
+    join lists into a string
+    """
+    res = sep.join(flatten(args))
+    return res
---
+++ tests/test_pycone.py
@@ -0,0 +1,9 @@
+from pycone import *
+
+
+def test_flatten():
+    assert flatten([1, 2, [3, [4, 5]]]) == [1, 2, 3, 4, 5]
+
+
+def test_list_join():
+    assert list_join(list("abc"), list("def"), sep=",") == "a,b,c,d,e,f"
